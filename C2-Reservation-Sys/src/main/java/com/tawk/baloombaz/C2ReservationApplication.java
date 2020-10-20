008022b703f98d39d404b6db0eba25599a5d0633
Jhonny <Jhonny.tawk@mcloudagency.com>
2020-10-21 01:43:27 +0300

    Initialize the project

---
+++ C2-Reservation-Sys/src/main/java/com/tawk/baloombaz/C2ReservationApplication.java
@@ -0,0 +1,13 @@
+package com.tawk.baloombaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2ReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C2ReservationApplication.class, args);
+    }
+
+}
