091ff28351793a50ec505fd851503dae94e16483
Jhonny <Jhonny.tawk@mcloudagency.com>
2020-10-21 21:33:41 +0300

    Adding User and User roles

---
+++ C2-Reservation-Sys/src/main/java/com/tawk/baloombaz/model/user/Role.java
@@ -0,0 +1,23 @@
+package com.tawk.baloombaz.model.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+@Document(collection = "role")
+public class Role {
+    @Id
+    private String id;
+
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING)
+    private String role;
+}
---
+++ C2-Reservation-Sys/src/main/java/com/tawk/baloombaz/model/user/UserRoles.java
@@ -0,0 +1,5 @@
+package com.tawk.baloombaz.model.user;
+
+public enum UserRoles {
+    ADMIN, PASSENGER
+}
